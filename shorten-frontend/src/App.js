215a261f35b0db1e3d095311f41ab778de218871
Kurmanbekov Ernur <erazavr85@gmail.com>
2020-02-27 20:18:02 +0600

    Написал frontend

---
+++ shorten-frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Shorten from "./container /Shorten /Shorten";
+
+const App = () => {
+    return (
+        <Shorten/>
+    );
+};
+
+export default App;
---
+++ shorten-frontend/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, compose} from 'redux';
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App';
+import reducer from "./store/reducer";
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+const app = (
+    <Provider store={store}>
+            <App/>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
+serviceWorker.unregister();
